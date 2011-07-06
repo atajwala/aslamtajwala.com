372e3fd8569c265998e5d099101968aa518da7c7
Aslam Tajwala <atajwala@gmail.com>
2011-07-05 22:05:21 -0700

    remaining files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AslamtajwalaCom::Application.config.secret_token = 'c47de6f1ccf593451a4e7306f1929830328eb5255f94a375c0e1b028cda411790034605b2150a1d2bfedd699a4f734f412a03611e8cfe53dc81dee1176ee3224'
